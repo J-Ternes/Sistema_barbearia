078bb8eba0e3ba781d9deffaf4bcbc443ea84aca
Jonathan Ternes <ternes.jonathangabriel@gmail.com>
2026-05-29 20:58:11 -0300

    feat: Apaguei EmailNaoEcontradoException

--- src/main/java/com/jonathandev/barberia/exception/EmailNaoEncontradoException.java
+++
@@ -1,7 +0,0 @@
-package com.jonathandev.barberia.exception;
-
-public class EmailNaoEncontradoException extends RuntimeException {
-    public EmailNaoEncontradoException() {
-        super();
-    }
-}
--- src/main/java/com/jonathandev/barberia/handler/GlobalExceptionHandler.java
+++ src/main/java/com/jonathandev/barberia/handler/GlobalExceptionHandler.java
@@ -2,7 +2,6 @@ package com.jonathandev.barberia.handler;
 
 import com.jonathandev.barberia.dto.ErrorResponseDto;
 import com.jonathandev.barberia.exception.EmailEncontradoException;
-import com.jonathandev.barberia.exception.EmailNaoEncontradoException;
 import com.jonathandev.barberia.exception.UsuarioNaoEncontradoException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,8 +23,4 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponseDto(404,"Usuário não cadastrado",LocalDateTime.now()));
     }
 
-    @ExceptionHandler(EmailNaoEncontradoException.class)
-    public ResponseEntity<ErrorResponseDto> EmailNaoEncontradoHandler(EmailNaoEncontradoException emailNaoEncontradoException){
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponseDto(404,"Email não cadastrado",LocalDateTime.now()));
-    }
 }
