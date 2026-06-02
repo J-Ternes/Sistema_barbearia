23f8480a2cad720b17975a8861b5479e0411493d
Jonathan Ternes <ternes.jonathangabriel@gmail.com>
2026-06-01 21:45:27 -0300

    feat: Adicionei @Valid no controller de ccriação da barbearia

--- src/main/java/com/jonathandev/barberia/controller/BarbeariaController.java
+++ src/main/java/com/jonathandev/barberia/controller/BarbeariaController.java
@@ -2,10 +2,13 @@ package com.jonathandev.barberia.controller;
 
 import com.jonathandev.barberia.dto.BarbeariaRegisterDto;
 import com.jonathandev.barberia.service.BarbeariaService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/barbearia")
@@ -15,15 +18,18 @@ public class BarbeariaController {
     private final BarbeariaService barbeariaService;
 
     @PostMapping
-    public ResponseEntity cadastrar(@RequestBody BarbeariaRegisterDto barbearia){
+    public ResponseEntity cadastrar(@RequestBody @Valid BarbeariaRegisterDto barbearia){
         barbeariaService.criarBarbearia(barbearia);
         return ResponseEntity.status(HttpStatus.CREATED).body(barbearia);
     }
 
     @GetMapping("/cadastradas")
     public ResponseEntity barbearias(){
-
         return ResponseEntity.status(HttpStatus.FOUND).body(barbeariaService.mostrarBarbearias());
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity delete(@PathVariable UUID id){
 
     }
 
