82b6f8144ac723539a9db1b3921516e1065c85d1
Jonathan Ternes <ternes.jonathangabriel@gmail.com>
2026-06-04 19:52:04 -0300

    @Size ára nomeServico

--- src/main/java/com/jonathandev/barberia/model/ServicosModel.java
+++ src/main/java/com/jonathandev/barberia/model/ServicosModel.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
@@ -23,6 +24,7 @@ public class ServicosModel {
 
     @Column(name = "nome_servico", nullable = false)
     @NotBlank(message = "O campo nomeServico é obrigatório")
+    @Size(max = 30)
     private String nomeServico;
 
     @Column(name = "preco_servico", nullable = false)
